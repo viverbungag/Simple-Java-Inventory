6456450da2b261e026ee6ccb31c34baa45bea2b7
viverbungag <viverbungag1@gmail.com>
2021-05-30 23:30:28 +0800

    initial add of files

---
+++ src/prog2exer11/Prog2Exer11.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog2exer11;
+
+/**
+ *
+ * @author Viver
+ */
+public class Prog2Exer11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CostumerForm form = new CostumerForm();
+        form.setVisible(true);
+    }
+    
+}
